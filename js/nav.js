b344a1885ce58d0c4af35392fb0b379a94bfe551
MikeJLeon <michaeljleon@live.com>
2020-07-16 20:33:21 -0700

    Added About, Added responsive styles

--- js/nav.js
+++ js/nav.js
@@ -9,7 +9,20 @@ function initializeNav() {
     window.setTimeout(showNavButtons.bind(element), current);
     current += 200;
   }
+  let ham = document.getElementById("rightNav");
+  ham.addEventListener("click", openNav.bind("burger"));
+  document.getElementById("content").addEventListener("click", openNav.bind("body"));
   window.setInterval(shaky, 2000);
+}
+function openNav(event) {
+  console.log(this);
+  let hamNav = document.getElementsByClassName("hamNav")[0];
+  if (hamNav.classList.contains("activeHam") && (this == "burger" || this == "body")) {
+    document.getElementById("content").removeEventListener("click", openNav.bind("body"));
+    hamNav.classList.remove("activeHam");
+  }else {
+    hamNav.classList.add("activeHam");
+  }
 }
 function shaky() {
   let buttons = document.getElementsByClassName("shaky");
