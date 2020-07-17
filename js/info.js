4acfd91ce9d7c6a7451d9874398b95f526e0870b
mikejleon <michaeljleon@live.com>
2020-07-17 15:51:32 -0700

    added screenshots to info page. modal not yet implemented.

--- js/info.js
+++ js/info.js
@@ -1,3 +1,15 @@
 window.onload = function () {
-    window.setTimeout(initializeNav, 500);
+    initializeInfo()
+    //window.setTimeout(initializeNav, 500);
 };
+
+function initializeInfo(){
+    let screenshots = document.getElementsByClassName("screenshot");
+    for(let screenshot of screenshots){
+        console.log(screenshot);
+        screenshot.addEventListener("click", modalOpen);
+    }
+}
+function modalOpen(){
+    console.log("Image clicked");
+}
