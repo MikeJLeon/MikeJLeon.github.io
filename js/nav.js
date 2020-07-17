8b45e361780e6fe5359131c0811568866e415260
MikeJLeon <michaeljleon@live.com>
2020-07-16 21:05:51 -0700

    Fixed some responsive bugs.

--- js/nav.js
+++ js/nav.js
@@ -11,14 +11,14 @@ function initializeNav() {
   }
   let ham = document.getElementById("rightNav");
   ham.addEventListener("click", openNav.bind("burger"));
-  document.getElementById("content").addEventListener("click", openNav.bind("body"));
+ //document.getElementById("content").addEventListener("click", openNav.bind("body"));
   window.setInterval(shaky, 2000);
 }
 function openNav(event) {
   console.log(this);
   let hamNav = document.getElementsByClassName("hamNav")[0];
   if (hamNav.classList.contains("activeHam") && (this == "burger" || this == "body")) {
-    document.getElementById("content").removeEventListener("click", openNav.bind("body"));
+    //document.getElementById("content").removeEventListener("click", openNav.bind("body"));
     hamNav.classList.remove("activeHam");
   }else {
     hamNav.classList.add("activeHam");
