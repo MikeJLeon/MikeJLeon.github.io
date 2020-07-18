26e3591ab7387c401e1efdcec983acdc1cfe9258
MikeJLeon <michaeljleon@live.com>
2020-07-17 22:16:23 -0700

    Modal can now open / close.

--- js/info.js
+++ js/info.js
@@ -7,9 +7,19 @@ function initializeInfo(){
     let screenshots = document.getElementsByClassName("screenshot");
     for(let screenshot of screenshots){
         console.log(screenshot);
-        screenshot.addEventListener("click", modalOpen);
+        screenshot.addEventListener("click", modalOpen.bind(null, screenshot));
     }
 }
-function modalOpen(){
-    console.log("Image clicked");
+function modalOpen(screenshot){
+    let modalImg = document.getElementsByClassName("modalImg")[0];
+    let modal = document.getElementsByClassName("infoModal")[0];
+    let modalExit = document.getElementsByClassName("exitModal")[0];
+    modalExit.addEventListener("click", modalClose.bind(null, modal, modalExit));
+    modalImg.src = screenshot.src;
+    console.log(screenshot.src);
+    modal.setAttribute("style", "display:block");
+}
+function modalClose(modal, modalExit){
+    modal.setAttribute("style", "display:none");
+    modalExit.removeEventListener("click", modalClose);
 }
